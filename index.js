4628fa53d62d3acd135dfa3d15f319a21f9ce208
rusty619 <rusty6916@gmail.com>
2022-03-15 22:28:32 -0400

    commit tree 040d41404e911d5b48b433feaa3a1d4d575e313e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a){
+    if(a < 42) return 42-a
+    else return a - 42;
+}
+
+function distanceFromHqInFeet(a){
+    let x  = distanceFromHqInBlocks(a) * 264
+    return x;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let s = distanceFromHqInFeet(start);
+    let d = distanceFromHqInFeet(destination);
+    return d-s;
+}
+
+function calculatesFarePrice(start, destination){
+    let total = distanceTravelledInFeet(start,destination)
+    if(total < 400){
+        return 0;
+    }
+    else if(total > 400 && total < 2000){
+        return (total-400) * 0.02;
+    }
+    else if(total > 2000 && total < 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
